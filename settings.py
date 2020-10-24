1cdf92fa416bd1a8c2c2e2beda26c01bd2417e24
Catry <catry.me@gmail.com>
2020-10-24 19:41:56 +0900

    delete print

--- settings.py
+++ settings.py
@@ -10,6 +10,4 @@ def check():
     if setting['github_id'] is None or setting['github_token'] is None or setting['search_month_range'] == 6314:
         print('Required settings are missing or search_month_range is 6314 over')
         exit(-1)
-    print(str(setting['search_topic'] or 'hacktoberfest'), int(
-        setting['search_month_range'] or 6), str(setting['search_location'] or 'Korea'))
     return str(setting['search_topic'] or 'hacktoberfest'), int(setting['search_month_range'] or 6), str(setting['search_location'] or 'Korea')
