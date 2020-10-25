1f4090474f07485d1ce08cb9ba95c42ec68e7a1d
Catry <catry.me@gmail.com>
2020-10-26 00:13:13 +0900

    change message

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ setting = env.dotenv_values(dotenv_path=env_path)
 
 def check():
     if setting['github_id'] is None or setting['github_token'] is None or setting['search_month_range'] == 6314:
-        print('Required settings are missing or search_month_range is 6314 over')
+        print('Required settings are missing or search month range is 6314 over')
         exit(-1)
     return str(setting['search_topic'] or 'hacktoberfest'), int(setting['search_month_range'] or 6), str(setting['search_location'] or 'Korea')
