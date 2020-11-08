f96b90db9e155e7d9d8042ae8bc50f304a9c8323
Harry Park <phg98@naver.com>
2020-11-08 16:22:17 +0900

    Add more tests

--- test_main.py
+++ test_main.py
@@ -12,12 +12,27 @@ def args():
     args.append('search_location=Korea')
     return args
     
-def test_main_found_1_result(args):
+def test_main_should_find_1_result(args):
     found_count = main.main(args)
     assert found_count == 1
 
-def test_main_found_no_result(args):
+def test_main_should_use_search_start_date(args):
     args.append('search_start_date=2020-11-02')
-    
+    found_count = main.main(args)
+    assert found_count == 0
+
+def test_main_should_use_search_day_range(args):
+    args.append('search_start_date=2020-11-02')
+    args.append('search_day_range=2')
+    found_count = main.main(args)
+    assert found_count == 1
+
+def test_main_should_use_search_location(args):
+    args.append('search_location=Japan')
+    found_count = main.main(args)
+    assert found_count == 0
+
+def test_main_should_use_search_topic(args):
+    args.append('search_topic=unlikely-existing-topic')
     found_count = main.main(args)
     assert found_count == 0
