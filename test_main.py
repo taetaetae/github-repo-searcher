35463a535b8a96f27977e8d5563404bdfcde650d
Harry Park <phg98@naver.com>
2020-11-08 16:03:33 +0900

    Add test for no result

--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
-import main, os
+import main, os, pytest
 
-def test_main():
+@pytest.fixture
+def args():
     args = []
     args.append('github_id='+os.environ['MY_GITHUB_ID'])
     args.append('github_token='+os.environ['MY_GITHUB_TOKEN'])
@@ -9,6 +10,14 @@ def test_main():
     args.append('search_day_range=1')
     args.append('search_start_date=2020-11-01')
     args.append('search_location=Korea')
+    return args
     
+def test_main_found_1_result(args):
     found_count = main.main(args)
     assert found_count == 1
+
+def test_main_found_no_result(args):
+    args.append('search_start_date=2020-11-02')
+    
+    found_count = main.main(args)
+    assert found_count == 0
